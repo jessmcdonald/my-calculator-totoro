1afb18588ff6175f2b6049a7d98340cddac39ac5
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2020-02-04 17:35:33 +0100

    calculator positioning

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,12 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      digits: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      buttons: [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "+", "-", "/", "*", "="],
       result: null
     };
   }
 
   render() {
-    const {digits} = this.state.digits
     return (
       <div className="App">
         <header className="App-header">
@@ -24,15 +23,23 @@ class App extends React.Component {
           </div>
           <h3>My Calculator</h3>
           <h1>TOTORO</h1>
-          <ul>
-          {this.state.digits.map(item => (
-            <Button key={item} digit={item}>{item}</Button>
-          ))}
-          </ul>
-          
-          <p>
-          <img src={totoro} alt="Totoro" height="600" />
-          </p>
+          <div className="Totoro">
+            <img src={totoro} alt="Totoro" height="600" />
+
+            <div className="Calculator">
+              <div className="Display-Screen">
+              <Screen/>
+              </div>
+              
+              <div className="Button-Pad">
+                <div className="Buttons">
+                  {this.state.buttons.map(item => (
+                    <Button key={item} digit={item}>{item}</Button>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
         </body>
       </div>
     );
