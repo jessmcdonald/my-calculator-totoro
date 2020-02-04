6187eaa37d359404e6be19762918bff13d33c676
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2020-02-04 16:19:26 +0100

    screen css

--- src/components/screen/screen.js
+++ src/components/screen/screen.js
@@ -1,1 +1,19 @@
 import './screen.css';
+import React from 'react';
+
+class Screen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this.props
+    }
+    
+    render() {
+        
+        return(
+                <div className="Screen-Div">1234567</div>
+            
+        )
+    }
+}
+
+export default Screen;
