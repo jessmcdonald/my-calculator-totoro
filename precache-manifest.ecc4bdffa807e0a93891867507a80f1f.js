67bf886ad0909dd19a6ac55a6643b36d0ee3e12a
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2020-02-05 16:36:46 +0100

    Updates

---
+++ precache-manifest.ecc4bdffa807e0a93891867507a80f1f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35da95e8c54e1dbea209f5c84542f9c1",
+    "url": "/my-calculator-totoro/index.html"
+  },
+  {
+    "revision": "2b609d6b607a30af6db9",
+    "url": "/my-calculator-totoro/static/css/main.7589b346.chunk.css"
+  },
+  {
+    "revision": "b498a227d3921be562e3",
+    "url": "/my-calculator-totoro/static/js/2.38d4dbce.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-calculator-totoro/static/js/2.38d4dbce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b609d6b607a30af6db9",
+    "url": "/my-calculator-totoro/static/js/main.f854f89c.chunk.js"
+  },
+  {
+    "revision": "5dcbcf02693329e0a9f9",
+    "url": "/my-calculator-totoro/static/js/runtime-main.b34d64da.js"
+  },
+  {
+    "revision": "624871e1b6172da8c089029daf0c632e",
+    "url": "/my-calculator-totoro/static/media/SevenSegment.624871e1.ttf"
+  },
+  {
+    "revision": "5b2d11e908671abdbd1f5ecbfc192a18",
+    "url": "/my-calculator-totoro/static/media/Spumoni.5b2d11e9.ttf"
+  },
+  {
+    "revision": "e7a270aa0b5eb096a38823c2d5a01029",
+    "url": "/my-calculator-totoro/static/media/background.e7a270aa.jpg"
+  },
+  {
+    "revision": "f93bb596019bba5f5c1fb0f1d0cf1643",
+    "url": "/my-calculator-totoro/static/media/totoro.f93bb596.png"
+  }
+]);
